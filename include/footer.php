45bf9dc35ed4f5c4962aa605e219ce37acae9637
sonaligupta84 <sonaligupta_mca22_24@its.edu.in>
2024-02-26 21:47:02 +0530

    project

---
+++ include/footer.php
@@ -0,0 +1,30 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <!-- <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                             <li>
+                                  <a href="#"> <img src="images/bitcoin.png" alt="CASH"> </a>
+                              </li>-->
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>Delhi, India.</p>
+                          <h5>Phone: +8801608414</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
+
